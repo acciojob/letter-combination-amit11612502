31a1ef8379225baf66385cd4e22d730165ce51b0
amit11612502 <38473703+amit11612502@users.noreply.github.com>
2023-06-18 23:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function letterCombinations(input_digit) {
   //Complete the function
+if (digits === "") {
+    return [];
+  }
+const phone = {
+	"1":"1",
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+	"0": "0",
+  };
+
+	const res = [];
+
+function backtrack(index, path) {
+    if (path.length === digits.length) {
+      res.push(path);
+      return;
+    }
+const letters = phone[digits[index]];
+    for (let i = 0; i < letters.length; i++) {
+      backtrack(index + 1, path + letters[i]);
+    }
+}
+	backtrack(0, "");
+  return res;
+	
 }
 
 module.exports = letterCombinations;
